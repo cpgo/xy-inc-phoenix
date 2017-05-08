220f6a1b7e09ef779500c0298ef9b3d208d91b05
Cassio Godinho <cassiopgodinho@gmail.com>
2017-05-08 18:53:38 -0300

    heroku configs

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,18 @@ use Mix.Config
 # which you typically run after static files are built.
 config :xy_inc, XyInc.Web.Endpoint,
   on_init: {XyInc.Web.Endpoint, :load_from_system_env, []},
+  url: [scheme: "https", host: "aqueous-taiga-52883.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :xy_inc, XyInc.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
+  # cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -61,4 +71,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
